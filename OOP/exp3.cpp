8a08021bfab2656cfeba680990212cc9416a2eb0
Shashwat Kelzarkar <shashwatkelzarkar@outlook.com>
2026-07-22 15:25:59 +0530

    added OOP practical programs

---
+++ OOP/exp3.cpp
@@ -0,0 +1,16 @@
+//write a program to convert float datatype number into integer and integer into float
+#include <iostream>
+using namespace std;
+
+int  main(){
+    float a = 2.34,d;
+    int b= 3,c;
+     c=(float)a;
+     b=(int)d;
+     cout << "float: "<< c<<endl;
+     cout <<"int: "<< b;
+    
+
+
+    return 0;
+}
